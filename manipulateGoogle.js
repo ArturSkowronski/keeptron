932b61ad22fcdb1044c10a4cca813bad53e349be
Artur Skowronski <a.skowronski@smartrecruiters.com>
2016-09-21 11:14:15 +0200

    First Raw Version

---
+++ manipulateGoogle.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+	var el1 = document.getElementsByClassName("gb_kd")[0]
+	var el2 = document.getElementsByClassName("gb_Xb")[0]
+	if(el1) el1.style.display="none"
+	if(el2) el2.style.display="none"
+};
