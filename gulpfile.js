ac18ace8ced1a83238c4d19235e6f5640beeaf44
Artur Skowronski <a.skowronski@smartrecruiters.com>
2017-02-19 16:26:05 +0100

    Add Gulp Support

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,14 @@
 
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
+const electronPacker = require("gulp-electron-packer");
 
 gulp.task('lint', () => {
     // ESLint ignores files with "node_modules" paths.
     // So, it's best to have gulp ignore the directory as well.
     // Also, Be sure to return the stream from the task;
     // Otherwise, the task may end before the stream has finished.
-    return gulp.watch(['**/*.js', '!node_modules/**', '!dist/**'])
+    return gulp.src(['**/*.js', '!node_modules/**', '!dist/**'])
     // eslint() attaches the lint output to the "eslint" property
     // of the file object so it can be used by other modules.
         .pipe(eslint())
@@ -20,6 +21,8 @@ gulp.task('lint', () => {
         .pipe(eslint.failAfterError());
 });
 
-gulp.task('default', ['lint'], function () {
-    // This will only run if the lint task is successful...
+gulp.task('default', ['lint']);
+
+gulp.task('watch', function() {
+    gulp.watch(['**/*.js', '!node_modules/**', '!dist/**'], ['lint']);
 });
