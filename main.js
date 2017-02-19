87f71fc225ef9815a2e0d9cc47fbd8cfd174726b
Artur Skowronski <a.skowronski@smartrecruiters.com>
2017-02-19 11:55:15 +0100

    ESLint included

--- main.js
+++ main.js
@@ -1,88 +1,88 @@
-const electron = require('electron')
-const menubar = require('menubar')
-const {Menu, Tray} = require('electron')
+'use strict';
+
+const electron = require('electron');
+const MenuBar = require('menubar');
+const {Menu, Tray} = require('electron');
 const AutoLaunch = require('auto-launch');
-const MenuTemplate = require('./src/clipboard.js').template
-const Keymap = require('./src/keymap.js')
-const ipc = electron.ipcMain;
+const MenuTemplate = require('./src/clipboard.js').template;
+const Keymap = require('./src/keymap.js');
+const winston = require('winston');
+const platform = require('os').platform();
 
 require('electron-reload')(__dirname);
 require('electron-debug')({showDevTools: false});
 
+let trayImage;
+const imageFolder = `${__dirname}/assets`;
 
-var platform = require('os').platform();  
-var trayImage;  
-var imageFolder = __dirname + '/assets';
-
-if (platform == 'darwin') {  
-  trayImage = imageFolder + '/keepTemplate.png';
+if (platform === 'darwin') {
+    trayImage = `${imageFolder}/keepTemplate.png`;
 }
 
 const menuBarConfiguration = {
-  width: 400, 
-  height: 600,
-  icon: __dirname + '/assets/keepTemplate.png',
-  preloadWindow: false
-}
+    width: 400,
+    height: 600,
+    icon: `${__dirname}/assets/keepTemplate.png`,
+    preloadWindow: false
+};
 
-const mb = menubar(menuBarConfiguration)
+const mb = MenuBar(menuBarConfiguration);
 
-mb.on('after-create-window', function ready () {
-  mb.window.focus();
-  var appPath = mb.app.getPath('exe').split('.app/Content')[0] + '.app';
+mb.on('after-create-window', () => {
+    mb.window.focus();
+    const appPath = `${mb.app.getPath('exe').split('.app/Content')[0]}.app`;
 
-  var keeptronAutostart = new AutoLaunch({
-    name: mb.app.getName(),
-    path: appPath,
-  });
+    const keeptronAutostart = new AutoLaunch({
+        name: mb.app.getName(),
+        path: appPath
+    });
 
-  keeptronAutostart.enable().then(function(isEnabled){
-   console.log(isEnabled)
- });
+    keeptronAutostart.enable().then(function(isEnabled) {
+        winston.info(isEnabled);
+    });
 
-  keeptronAutostart.isEnabled()
-  .then(function(isEnabled){
-   console.log(isEnabled)
-   if(isEnabled){
-    return;
-  }
-  keeptronAutostart.enable();
-})
-  .catch(function(err){
-   console.log(err)
- });
-})
+    keeptronAutostart.isEnabled()
+        .then(function(isEnabled) {
+            winston.info(isEnabled);
+            if (isEnabled) {
+                return;
+            }
+            keeptronAutostart.enable();
+        })
+        .catch(function(err) {
+            winston.err(err);
+        });
+});
 
 mb.app.on('ready', () => {
-  var appIcon = new Tray(trayImage);
+    const appIcon = new Tray(trayImage);
 
-  if (platform == "darwin") {  
-    appIcon.setPressedImage(imageFolder + '/keepHighlight.png');
-  }
+    if (platform === "darwin") {
+        appIcon.setPressedImage(`${imageFolder}/keepHighlight.png`);
+    }
 
-  Menu.setApplicationMenu(Menu.buildFromTemplate(MenuTemplate(mb.app)));
-  Keymap.initializeKeymap(electron.globalShortcut, 
-    [
-    {shorcut: 'CommandOrControl+Alt+K', function: mb.showWindow}
-    ]
-    )
-})
+    Menu.setApplicationMenu(Menu.buildFromTemplate(MenuTemplate(mb.app)));
+    Keymap.initializeKeymap(electron.globalShortcut,
+        [
+    {shortcut: 'CommandOrControl+Alt+K', function: mb.showWindow}
+        ]
+    );
+});
 
 mb.on('after-show', () => {
-  electron.globalShortcut.register('Esc', mb.hideWindow)
-})
-
+    electron.globalShortcut.register('Esc', mb.hideWindow);
+});
 
 mb.on('after-hide', () => {
-  electron.globalShortcut.unregister('Esc')
-})
+    electron.globalShortcut.unregister('Esc');
+});
 
 mb.app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    mb.app.quit()
-  }
-})
+    if (process.platform !== 'darwin') {
+        mb.app.quit();
+    }
+});
 
 mb.app.on('will-quit', () => {
-  electron.globalShortcut.unregisterAll()
-})
+    electron.globalShortcut.unregisterAll();
+});
--- src/keymap.js
+++ src/keymap.js
@@ -1,18 +1,21 @@
+'use strict';
+
+const winston = require('winston');
+
 exports.initializeKeymap = (globalShortcut, keymap) => {
-	
-	keymap.forEach((x) => {
-		if (!x.shorcut || !x.function) {
-			console.log(`Keeptron: Shortcut ${x.shorcut} not registered for function ${x.function}`)
-			return;
-		}
 
-		var ret = globalShortcut.register(x.shorcut, x.function)
+    keymap.forEach((x) => {
+        if (!x.shortcut || !x.function) {
+            winston.info(`Keeptron: Shortcut ${x.shortcut} not registered for function ${x.function}`);
+            return;
+        }
+        const ret = globalShortcut.register(x.shorcut, x.function);
 
-		if (ret && globalShortcut.isRegistered(x.shorcut)) {
-			console.log(`Keeptron: Shortcut ${x.shorcut} registered properly`)
-		} else {
-			console.log(`Keeptron: Shortcut ${x.shorcut} not registered`)
-		}
-		
-	})
-}
+        if (ret && globalShortcut.isRegistered(x.shortcut)) {
+            winston.info(`Keeptron: Shortcut ${x.shortcut} registered properly`);
+        } else {
+            winston.info(`Keeptron: Shortcut ${x.shortcut} not registered`);
+        }
+
+    });
+};
